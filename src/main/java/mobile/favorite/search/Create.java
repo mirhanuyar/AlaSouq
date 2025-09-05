4fa3decb1d030ff2d0bfba8ea58a2a64ecea2288
Nevruz <demirnevruz0@gamil.com>
2025-09-05 12:33:32 +0300

    mobile

--- src/main/java/mobile/favorite/search/Create.java
+++ src/main/java/mobile/favorite/search/Create.java
@@ -1,9 +1,6 @@
 package mobile.favorite.search;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -21,41 +18,22 @@ public class Create {
     public static WebDriverWait wait;
 
     public static void main(String[] args) {
-        setUpDriver();
+
         try {
-            openRegistrationPage();
+            setUpDriver();
             Thread.sleep(2000);
-
-            clickSaveAppSettings();
+            saveModal();
             Thread.sleep(2000);
-
-            fullScreen();
-            Thread.sleep(2000);
-
-            clickTabButtonMyAccount();
-            Thread.sleep(2000);
-
-            clickLoginButton();
-            Thread.sleep(2000);
-
             fillForm();
             Thread.sleep(2000);
-
             submitForm();
             Thread.sleep(2000);
-
             clickButtonHome();
             Thread.sleep(2000);
-
             clickSearchInput();
             Thread.sleep(2000);
-
             clickRealEstate();
             Thread.sleep(2000);
-
-            clickSaveSearch();
-            Thread.sleep(3000);
-
             clickSearchNameInput();
             Thread.sleep(2000);
 
@@ -65,60 +43,69 @@ public class Create {
         }
     }
 
-    public static void setUpDriver() {
-        Map<String, Object> deviceMetrics = new HashMap<>();
-        deviceMetrics.put("width", 500);
-        deviceMetrics.put("height", 800);
-        deviceMetrics.put("pixelRatio", 3.0);
-
-        Map<String, Object> mobileEmulation = new HashMap<>();
-        mobileEmulation.put("deviceMetrics", deviceMetrics);
-        mobileEmulation.put("userAgent", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) " +
-                "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1");
+    public static void setUpDriver() throws InterruptedException {
 
         ChromeOptions options = new ChromeOptions();
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "iPhone X");
         options.setExperimentalOption("mobileEmulation", mobileEmulation);
-
         driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://m.alasouq.com/en/home");
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+    }
+    public static void saveModal() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement button = driver.findElement(By.cssSelector("ion-button.button-outline"));
+        button.click();
+        Thread.sleep(2000);
+
     }
 
-    public static void openRegistrationPage() {
-        driver.get("https://m.alasouq.com");
-    }
-
-    public static void clickSaveAppSettings() throws InterruptedException {
-        clickElement(By.xpath("//ion-button[text()='Default']"));
-    }
-
-    public static void fullScreen() {
-        driver.manage().window().fullscreen();
-    }
-
-    public static void clickTabButtonMyAccount() throws InterruptedException {
-        clickElement(By.id("btn-my-account"));
-    }
-
-    public static void clickLoginButton() throws InterruptedException {
-        clickElement(By.id("login-click"));
-    }
 
     public static void fillForm() throws InterruptedException {
-        WebElement emailField = driver.findElement(By.id("ion-input-0"));
-        emailField.sendKeys("yakup.backoffice@solidsoft.com.tr");
+        WebElement userIcon = driver.findElement(By.id("icon-person-outline"));
+        userIcon.click();
+        Thread.sleep(1000);
+
+        WebElement loginButton = driver.findElement(
+                By.id("login-click"));
+
+        loginButton.click();
+        Thread.sleep(2000);
+
+        WebElement emailField = driver.findElement(
+                By.cssSelector("input[type='email'].native-input")
+        );
+
+        emailField.sendKeys("onder.backoffice@solidsoft.com.tr");
         Thread.sleep(3000);
-        WebElement currentPasswordField = driver.findElement(By.id("ion-input-1"));
+
+        WebElement currentPasswordField = driver.findElement(By.cssSelector("input[type='password']"));
         currentPasswordField.sendKeys("admin");
         Thread.sleep(3000);
     }
 
     public static void submitForm() throws InterruptedException {
-        clickElement(By.id("btn-lgn-email"));
+        WebElement submitIcon = driver.findElement(By.id("btn-lgn-email"));
+        submitIcon.click();
         Thread.sleep(2000);
     }
 
     public static void clickButtonHome() throws InterruptedException {
-        clickElement(By.id("btn-search"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement logoIcon = wait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector("ion-icon.logo")
+        ));
+        try {
+            logoIcon.click();
+        } catch (Exception e) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", logoIcon);
+        }
+
     }
 
     public static void clickSearchInput() throws InterruptedException {
@@ -132,19 +119,55 @@ public class Create {
     public static void clickRealEstate() throws InterruptedException {
         WebElement realEstate = driver.findElement(By.xpath("//ion-item[.//h3[text()='Real Estate']]"));
         realEstate.click();
+        Thread.sleep(2000);
     }
 
-    public static void clickSaveSearch() throws InterruptedException {
-        WebElement search = driver.findElement(By.id("save-search-modal"));
-        search.click();
+
+    public static void clickSearchNameInput() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        try {
+            // Modalı aç (sadece bir kez)
+            WebElement modalButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("save-search-modal")));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", modalButton);
+
+            Thread.sleep(500); // Modal açılması için kısa bekleme
+
+            // Input alanı (normal DOM veya Shadow DOM)
+            WebElement input;
+            try {
+                input = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("ion-input-2")));
+            } catch (TimeoutException e) {
+                WebElement shadowHost = wait.until(ExpectedConditions.presenceOfElementLocated(
+                        By.cssSelector("modal-shadow-host-selector")
+                ));
+                input = (WebElement) ((JavascriptExecutor) driver)
+                        .executeScript("return arguments[0].shadowRoot.querySelector('input')", shadowHost);
+            }
+
+            // Input değerini JS ile ata ve input event tetikle
+            ((JavascriptExecutor) driver).executeScript(
+                    "arguments[0].value='Test'; arguments[0].dispatchEvent(new Event('input'));", input
+            );
+
+            // Kaydet butonunu görünür yap
+            WebElement saveButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("btn-save-search")));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", saveButton);
+
+            // Kaydet butonuna tıkla
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", saveButton);
+
+            System.out.println("Input değeri başarıyla atandı ve kaydedildi.");
+        } catch (Exception e) {
+            System.out.println("Bir hata oluştu: " + e.getMessage());
+        }
     }
 
-    public static void clickSearchNameInput() throws InterruptedException {
-        WebElement search = driver.findElement(By.id("favorite-search-name"));
-        search.click();
-        Thread.sleep(5000);
-        search.sendKeys("TEST");
-    }
+
+
+
+
+
 
     public static void clickElement(By locator) throws InterruptedException {
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
