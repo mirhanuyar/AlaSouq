20c7b512270b3bee2b804105a8d85453395330ec
Nevruz <demirnevruz0@gamil.com>
2025-09-10 17:43:27 +0300

    mobile

--- src/main/java/mobile/advert/create/real/estate/Building.java
+++ src/main/java/mobile/advert/create/real/estate/Building.java
@@ -1,37 +1,39 @@
 package mobile.advert.create.real.estate;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+import java.awt.event.InputEvent;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
-
 
 public class Building {
     public static WebDriver driver;
     public static WebDriverWait wait;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
         setUpDriver();
         try {
-            openRegistrationPage();
             Thread.sleep(2000);
-            clickSaveAppSettings();
-            Thread.sleep(2000);
-            Thread.sleep(2000);
-            clickCreateAdvertButton();
-            Thread.sleep(2000);
+            saveModal();
+            Thread.sleep(1000);
             fillForm();
-            Thread.sleep(2000);
-            signIn();
+            Thread.sleep(1000);
+            submitForm();
             Thread.sleep(2000);
             clickRealEstate();
             Thread.sleep(2000);
+            clickCreateAdvertButton();
+            Thread.sleep(1000);
             chooseCategory();
             Thread.sleep(2000);
             clickForRentCategory();
@@ -42,87 +44,101 @@ public class Building {
             Thread.sleep(2000);
             enterPrice();
             Thread.sleep(2000);
+            clickBody();
+            Thread.sleep(2000);
             scrollScreen(300);
             Thread.sleep(2000);
             //uploadPhotodnl();
             Thread.sleep(2000);
             scrollScreen(400);
             Thread.sleep(2000);
-            KeyboardAccessoryOkButton();
-            Thread.sleep(2000);
             clickNextButton2();
             Thread.sleep(2000);
             clickAddressInformation();
+            Thread.sleep(2000);
+            clickProvince();
+            Thread.sleep(2000);
+            clickDistrict();
+            Thread.sleep(2000);
+            clickNeighbourhood();
             Thread.sleep(2000);
             scrollScreen(1000);
             Thread.sleep(2000);
             clickNextButton3();
             Thread.sleep(2000);
-            selectMap();
+            selectMapMobile();
+            Thread.sleep(2000);
+            clickVisibleContinueButton(driver);
+            Thread.sleep(2000);
+            photoselect();
+            Thread.sleep(2000);
+            clickNextButtonpreview();
+            Thread.sleep(2000);
+            buydopingtButton();
             Thread.sleep(2000);
             scrollScreen(1000);
             Thread.sleep(2000);
-            clickBuyDopingButton();
-
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-
-    public static void setUpDriver() {
-        Map<String, Object> deviceMetrics = new HashMap<>();
-        deviceMetrics.put("width", 500);
-        deviceMetrics.put("height", 800);
-        deviceMetrics.put("pixelRatio", 3.0);
-
-        Map<String, Object> mobileEmulation = new HashMap<>();
-        mobileEmulation.put("deviceMetrics", deviceMetrics);
-        mobileEmulation.put("userAgent", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) " +
-                "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1");
-
+    public static void setUpDriver() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "iPhone X");
         options.setExperimentalOption("mobileEmulation", mobileEmulation);
-
         driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://m.alasouq.com/en/home");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-
-    public static void openRegistrationPage() throws InterruptedException {
-        driver.get("https://m.alasouq.com");
-    }
-
-    public static void clickSaveAppSettings() throws InterruptedException {
-        WebElement button = driver.findElement(By.xpath("//ion-button[text()='Default']"));
+    public static void saveModal () throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement button = driver.findElement(By.cssSelector("ion-button.button-outline"));
         button.click();
-    }
-
-    public static void clickCreateAdvertButton() throws InterruptedException {
-        WebElement createAdvert = driver.findElement(By.id("btn-construct"));
-        createAdvert.click();
+        Thread.sleep(2000);
     }
 
     public static void fillForm() throws InterruptedException {
-        WebElement emailField = driver.findElement(By.id("ion-input-0"));
-        emailField.sendKeys("yakup.user@solidsoft.com.tr");
+        WebElement loginButton = driver.findElement(By.id("btn-my-account"));
+        loginButton.click();
+        Thread.sleep(2000);
+
+        WebElement login= driver.findElement(By.id("login-click"));
+        login.click();
+        Thread.sleep(2000);
+
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email'].native-input"));
+        emailField.sendKeys("tajdin.gurdal@solidsoft.com.tr");
         Thread.sleep(3000);
-        WebElement currentPasswordField = driver.findElement(By.id("ion-input-1"));
+
+        WebElement currentPasswordField = driver.findElement(By.cssSelector("input[type='password']"));
         currentPasswordField.sendKeys("admin");
         Thread.sleep(3000);
     }
 
-    public static void signIn() throws InterruptedException {
-        WebElement clickSignIn;
-        clickSignIn = driver.findElement(By.id("btn-lgn-email"));
-        clickSignIn.click();
-        Thread.sleep(1000);
-        driver.manage().window().fullscreen();
+    public static void submitForm() throws InterruptedException {
+        WebElement submitIcon = driver.findElement(By.id("btn-lgn-email"));
+        submitIcon.click();
+        Thread.sleep(2000);
+        Thread.sleep(500);
     }
 
-    public static void clickRealEstate() throws InterruptedException {
-        WebElement realEstateCard = driver.findElement(By.id("icon-chevron-forward-outline"));
-        realEstateCard.click();
+    public static void clickRealEstate()throws InterruptedException {
+        WebElement btn = driver.findElement(By.cssSelector("ion-fab-button#btn-construct"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", btn);
+        js.executeScript("arguments[0].click();", btn);
+    }
+
+    public static void clickCreateAdvertButton()throws InterruptedException {
+        WebElement realEstateItem = wait.until(
+                ExpectedConditions.elementToBeClickable(By.xpath("//ion-item[contains(., 'Real Estate')]"))
+        );
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();", realEstateItem);
     }
 
     public static void chooseCategory() throws InterruptedException {
@@ -131,10 +147,11 @@ public class Building {
         js.executeScript("arguments[0].click();", item);
     }
 
-    public static void clickForRentCategory() {
+    public static void clickForRentCategory()throws InterruptedException {
         WebElement item = driver.findElement(By.xpath("//ion-item[contains(., 'For Rent')]"));
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", item);
+        Thread.sleep(500);
     }
 
     public static void titleInput() throws InterruptedException {
@@ -142,6 +159,7 @@ public class Building {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", input);
         input.sendKeys("My Title");
+        Thread.sleep(1000);
     }
 
     public static void enterDescription() throws InterruptedException {
@@ -154,42 +172,12 @@ public class Building {
     public static void enterPrice() throws InterruptedException {
         WebElement price = driver.findElement(By.id("ion-input-3"));
         price.sendKeys("10000");
+        Thread.sleep(500);
     }
-
-
-    /*public static void uploadPhotodnl() throws InterruptedException, AWTException {
-        String[] photoNames = {"download.jpeg", "download (1).jpeg", "download (2).jpeg"};
-        String basePath = "src/main/java/web/_images/";
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-
-        WebElement addImageButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".add-new-image")));
-        addImageButton.click();
-
-        WebElement fileInput = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("input[type='file']")));
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].style.display = 'block'; arguments[0].style.visibility = 'visible'; arguments[0].style.opacity = 1;", fileInput);
-
-        for (String name : photoNames) {
-            String filePath = Paths.get(basePath + name).toAbsolutePath().toString();
-            System.out.println("Yükleniyor: " + filePath);
-            fileInput.sendKeys(filePath);
-            Thread.sleep(1000);
-        }
-
-        Robot robot = new Robot();
-        robot.delay(2000);
-        robot.keyPress(KeyEvent.VK_ESCAPE);
-        robot.keyRelease(KeyEvent.VK_ESCAPE);
-
-    }*/
-
-    public static void KeyboardAccessoryOkButton()  throws InterruptedException {
-        WebElement okButton = driver.findElement(
-                By.xpath("//ion-item[.//ion-text[text()='Ok']]")
-        );
-        okButton.click();
+    public static void clickBody()throws InterruptedException {
+        WebElement body = driver.findElement(By.tagName("body"));
+        new Actions(driver).moveToElement(body, 10, 10).click().perform();
+        Thread.sleep(1000);
     }
 
     public static void clickNextButton2() throws InterruptedException {
@@ -198,35 +186,129 @@ public class Building {
     }
 
     public static void clickAddressInformation() throws InterruptedException {
-        WebElement nextButton = driver.findElement(By.id("btn-current-location"));
+        WebElement nextButton = driver.findElement(By.id("btn-want-choose"));
         nextButton.click();
+    }
+
+    public static void clickProvince() throws InterruptedException {
+        WebElement provinceSelect = driver.findElement(By.id("breakdown-selected-second"));
+        provinceSelect.click();
+        Thread.sleep(1000);
+        String province = "Babil";
+        WebElement provinceOption = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//ion-radio[normalize-space(text())='" + province + "']"))
+        );
+        provinceOption.click();
+    }
+    public static void clickDistrict() throws InterruptedException {
+        WebElement districtDropdown = driver.findElement(By.id("breakdown-selected-third"));
+        districtDropdown.click();
+        Thread.sleep(1000);
+
+        String district = "Al Hashimiya";
+        WebElement provinceOption = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//ion-radio[normalize-space(text())='" + district + "']"))
+        );
+        provinceOption.click();
+        Thread.sleep(500);
+
+    }
+
+    public static void clickNeighbourhood() throws InterruptedException {
+        WebElement neighbourhoodDropdown = driver.findElement(By.id("breakdown-selected-forth"));
+        neighbourhoodDropdown.click();
+        Thread.sleep(2000);
+
+        String neighbourhood = "Al Qasim";
+        WebElement neighbourhoodOption = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//ion-radio[normalize-space(text())='" + neighbourhood + "']")
+        ));
+        neighbourhoodOption.click();
+        Thread.sleep(500);
     }
 
     public static void clickNextButton3() throws InterruptedException {
         WebElement nextButton = driver.findElement(By.id("btn-continue-communication"));
         nextButton.click();
-    }
-
-    public static void selectMap() throws InterruptedException {
-        WebElement mapDiv = driver.findElement(By.cssSelector("div[style*='z-index: 3'][style*='position: absolute']"));
-        Actions actions = new Actions(driver);
-        actions.moveToElement(mapDiv).click().perform();
-    }
-
-
-    public static void clickBuyDopingButton() throws InterruptedException {
-        WebElement buyButton = driver.findElement(By.xpath("//button[contains(text(),'Satın Al')]"));
-        buyButton.click();
-    }
-
-    public static void clickElement(By locator) throws InterruptedException {
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
-        element.click();
         Thread.sleep(1000);
     }
+
+    public static void selectMapMobile() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        WebElement mapDiv = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.cssSelector("div[style*='z-index: 3'][style*='position: absolute']"))
+        );
+        Point location = mapDiv.getLocation();
+        Dimension size = mapDiv.getSize();
+        int centerX = location.getX() + size.getWidth() / 2;
+        int centerY = location.getY() + size.getHeight() / 2;
+        new Actions(driver).moveByOffset(centerX, centerY).click().perform();
+        new Actions(driver).moveByOffset(-centerX, -centerY).perform();
+
+        WebElement nextButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("btn-continue-address")));
+        scrollScreen(300);
+        try {
+            nextButton.click();
+        } catch (ElementNotInteractableException e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", nextButton);
+        }
+        Thread.sleep(1000);
+    }
+
+    public static void clickVisibleContinueButton(WebDriver driver) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        List<WebElement> buttons = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(
+                By.id("btn-continue-communication")
+        ));
+        for (WebElement btn : buttons) {
+            if (btn.isDisplayed() && btn.isEnabled()) {
+                ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", btn);
+                ((JavascriptExecutor) driver).executeScript("arguments[0].click();", btn);
+
+                return;
+            }
+        }
+        throw new RuntimeException();
+    }
+
+    public static void photoselect() throws InterruptedException {
+        WebElement selectButton = driver.findElement(By.id("btn-continue-photo-select"));
+        selectButton.click();
+        Thread.sleep(1000);
+
+        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//button[span[text()='set-address.alert-next-button']]")
+        ));
+        button.click();
+
+    }
+    public static void clickNextButtonpreview() throws InterruptedException {
+        WebElement nextButton = driver.findElement(By.id("checkbox-accept-purchase-agreement"));
+        nextButton.click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("btn-continue")).click();
+    }
+    public static void buydopingtButton() throws InterruptedException {
+        WebElement buydoping = driver.findElement(By.id("btn-offer-buy"));
+        buydoping.click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.id("btn-return-home-page")).click();
+    }
+
 
     public static void scrollScreen(int pixels) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0, arguments[0]);", pixels);
+    }
+
+    public static void clickElement(WebElement element) {
+        try {
+            element.click();
+        } catch (ElementNotInteractableException e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+        }
     }
 }
