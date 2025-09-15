b20539d4f274dec55d20af133737dfb2c31227cf
Nevruz <demirnevruz0@gamil.com>
2025-09-15 10:22:58 +0300

    mobile

--- src/main/java/mobile/mail/Login.java
+++ src/main/java/mobile/mail/Login.java
@@ -1,40 +1,89 @@
 package mobile.mail;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login {
-    public static void main(String[] args) {
 
 
+    public static WebDriver driver;
+    public static WebDriverWait wait;
 
-        WebDriver driver = new ChromeDriver();
+
+    public static void main(String[] args) throws InterruptedException {
+
+        setUpDriver();
 
         try {
-            driver.get("https://m.alasouq.com/home");
-
             Thread.sleep(2000);
-            WebElement username = driver.findElement(By.xpath("//*[@id=\"ion-input-0\"]"));
+            saveModal();
+            Thread.sleep(1000);
+            fillForm();
+            Thread.sleep(1000);
+            submitForm();
+            Thread.sleep(1000);
 
-            Thread.sleep(2000);
-            WebElement password = driver.findElement(By.xpath("//*[@id=\"ion-input-1\"]"));
 
-            Thread.sleep(2000);
-            username.sendKeys("yakup.backoffice@solidsoft.com.tr");
-
-            Thread.sleep(2000);
-            password.sendKeys("admin");
-
-            Thread.sleep(2000);
-            WebElement loginButton = driver.findElement(By.id("btn-lgn-email"));
-            loginButton.click();
-
-            Thread.sleep(3000);
-
-        } catch (Exception e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-}
+
+
+
+    public static void setUpDriver() throws InterruptedException {
+
+
+        ChromeOptions options = new ChromeOptions();
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "iPhone X");
+        options.setExperimentalOption("mobileEmulation", mobileEmulation);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://m.alasouq.com/en/home");
+    }
+
+    public static void saveModal () throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement button = driver.findElement(By.cssSelector("ion-button.button-outline"));
+        button.click();
+        Thread.sleep(2000);
+
+    }
+
+    public static void fillForm() throws InterruptedException {
+        WebElement userIcon = driver.findElement(By.id("icon-person-outline"));
+        userIcon.click();
+        Thread.sleep(1000);
+
+        WebElement loginButton = driver.findElement(
+                By.id("login-click"));
+
+        loginButton.click();
+        Thread.sleep(2000);
+
+        WebElement emailField = driver.findElement(
+                By.cssSelector("input[type='email'].native-input")
+        );
+
+        emailField.sendKeys("onder.backoffice@solidsoft.com.tr");
+        Thread.sleep(3000);
+
+        WebElement currentPasswordField = driver.findElement(By.id("ion-input-1"));
+        currentPasswordField.sendKeys("admin");
+        Thread.sleep(3000);
+    }
+
+    public static void submitForm() throws InterruptedException {
+        WebElement submitIcon = driver.findElement(By.id("btn-lgn-email"));
+        submitIcon.click();
+        Thread.sleep(2000);
+    }
+
+    }
+
