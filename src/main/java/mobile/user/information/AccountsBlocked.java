3615d5820f53efc08ac623f2e2eabcc77a2ad956
Nevruz <demirnevruz0@gamil.com>
2025-09-15 11:58:32 +0300

    mobile

--- src/main/java/mobile/user/information/AccountsBlocked.java
+++ src/main/java/mobile/user/information/AccountsBlocked.java
@@ -5,19 +5,22 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AccountsBlocked {
     public static WebDriver driver;
     public static WebDriverWait wait;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         setUpDriver();
         try {
-            openRegistrationPage();
+            saveModal();
             Thread.sleep(2000);
             fillForm();
             Thread.sleep(2000);
@@ -33,40 +36,61 @@ public class AccountsBlocked {
     }
 
 
-    public static void setUpDriver() {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    public static void setUpDriver() throws InterruptedException {
+
+
+        ChromeOptions options = new ChromeOptions();
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "iPhone X");
+        options.setExperimentalOption("mobileEmulation", mobileEmulation);
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://m.alasouq.com/en/home");
     }
 
-    public static void openRegistrationPage() {
-        driver.get("http://localhost:4200/home");
+    public static void saveModal () throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement button = driver.findElement(By.cssSelector("ion-button.button-outline"));
+        button.click();
+        Thread.sleep(2000);
+
     }
 
     public static void fillForm() throws InterruptedException {
-        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//span[contains(@class, 'sc-ion-label-ios') and text()='Giriş Yap']")));
-        loginButton.click();
+        WebElement userIcon = driver.findElement(By.id("icon-person-outline"));
+        userIcon.click();
         Thread.sleep(1000);
 
-        WebElement emailField = driver.findElement(By.id("ion-input-0"));
-        emailField.sendKeys("yakup.backoffice@solidsoft.com.tr");
+        WebElement loginButton = driver.findElement(
+                By.id("login-click"));
+
+        loginButton.click();
+        Thread.sleep(2000);
+
+        WebElement emailField = driver.findElement(
+                By.cssSelector("input[type='email'].native-input")
+        );
+
+        emailField.sendKeys("onder.backoffice@solidsoft.com.tr");
         Thread.sleep(3000);
+
         WebElement currentPasswordField = driver.findElement(By.id("ion-input-1"));
-        currentPasswordField.sendKeys("admin6565");
+        currentPasswordField.sendKeys("admin");
         Thread.sleep(3000);
     }
 
     public static void submitForm() throws InterruptedException {
-        clickElement(By.id("btn-lgn-email"));
+        WebElement submitIcon = driver.findElement(By.id("btn-lgn-email"));
+        submitIcon.click();
         Thread.sleep(2000);
     }
 
     public static void userInformation() throws InterruptedException {
-        clickElement(By.id("link-account-info"));
+        driver.findElement(By.id("link-dashboard")).click();
     }
 
     public static void blockedAccounts() throws InterruptedException {
-        clickElement(By.id("blocked-account-link"));
+        driver.findElement(By.id("blocked-account-link")).click();
     }
 
     public static void clickElement(By locator) throws InterruptedException {
