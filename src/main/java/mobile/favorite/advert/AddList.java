9d6edf88c17b589f8fa405e6c8f8b24cfec5360d
Nevruz <demirnevruz0@gamil.com>
2025-09-16 18:50:02 +0300

    mobile

--- src/main/java/mobile/favorite/advert/AddList.java
+++ src/main/java/mobile/favorite/advert/AddList.java
@@ -10,30 +10,29 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static mobile.categories.CategoryControls.clickSearch;
+import static mobile.favorite.advert.Delete.clickAdvert;
+import static mobile.favorite.advert.Notifications.fillForm;
+import static mobile.favorite.advert.Notifications.submitForm;
+import static mobile.message.ChatWithAdvertOwner.clickAllAds;
+import static mobile.message.ChatWithAdvertOwner.clickCategories;
+import static web.favorite.search.ControlSearch.clickFavorite;
+
 public class AddList {
     public static WebDriver driver;
     public static WebDriverWait wait;
+    private static List<String> favoriteAdverts = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws InterruptedException {
+
         setUpDriver();
         try {
-            openRegistrationPage();
-            Thread.sleep(2000);
-
-            clickSaveAppSettings();
-            Thread.sleep(2000);
-
-            fullScreen();
-            Thread.sleep(2000);
-
-            clickTabButtonMyAccount();
-            Thread.sleep(2000);
-
-            clickLoginButton();
+            saveModal();
             Thread.sleep(2000);
 
             fillForm();
@@ -69,107 +68,117 @@ public class AddList {
         }
     }
 
-    public static void setUpDriver() {
-        Map<String, Object> deviceMetrics = new HashMap<>();
-        deviceMetrics.put("width", 500);
-        deviceMetrics.put("height", 800);
-        deviceMetrics.put("pixelRatio", 3.0);
-
-        Map<String, Object> mobileEmulation = new HashMap<>();
-        mobileEmulation.put("deviceMetrics", deviceMetrics);
-        mobileEmulation.put("userAgent", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) " +
-                "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1");
+    public static void setUpDriver() throws InterruptedException {
 
         ChromeOptions options = new ChromeOptions();
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "iPhone X");
         options.setExperimentalOption("mobileEmulation", mobileEmulation);
-
         driver = new ChromeDriver(options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+        driver.get("https://m.alasouq.com/en/home");
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    public static void openRegistrationPage() {
-        driver.get("https://m.alasouq.com");
+    public static void saveModal () throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement button = driver.findElement(By.cssSelector("ion-button.button-outline"));
+        button.click();
+        Thread.sleep(2000);
     }
 
-    public static void clickSaveAppSettings() {
-        clickElement(By.xpath("//ion-button[text()='Default']"));
-    }
 
-    public static void fullScreen() {
-        driver.manage().window().fullscreen();
-    }
+        public static void fillForm () throws InterruptedException {
+            WebElement userIcon = driver.findElement(By.id("icon-person-outline"));
+            userIcon.click();
+            Thread.sleep(1000);
 
-    public static void clickTabButtonMyAccount() {
-        clickElement(By.id("btn-my-account"));
-    }
+            WebElement loginButton = driver.findElement(
+                    By.id("login-click"));
 
-    public static void clickLoginButton() {
-        clickElement(By.id("login-click"));
-    }
+            loginButton.click();
+            Thread.sleep(2000);
 
-    public static void fillForm() throws InterruptedException {
-        WebElement emailField = driver.findElement(By.id("ion-input-0"));
-        emailField.sendKeys("yakup.backoffice@solidsoft.com.tr");
-        Thread.sleep(1000);
+            WebElement emailField = driver.findElement(
+                    By.cssSelector("input[type='email'].native-input")
+            );
 
-        WebElement currentPasswordField = driver.findElement(By.id("ion-input-1"));
-        currentPasswordField.sendKeys("admin");
-        Thread.sleep(1000);
-    }
+            emailField.sendKeys("onder.backoffice@solidsoft.com.tr");
+            Thread.sleep(3000);
 
-    public static void submitForm() {
-        clickElement(By.id("btn-lgn-email"));
-    }
+            WebElement currentPasswordField = driver.findElement(By.id("ion-input-1"));
+            currentPasswordField.sendKeys("admin");
+            Thread.sleep(3000);
+        }
 
-    public static void clickSearch() throws InterruptedException {
-        WebElement search = driver.findElement(By.id("btn-search"));
-        search.click();
-    }
+        public static void submitForm () throws InterruptedException {
+            WebElement submitIcon = driver.findElement(By.id("btn-lgn-email"));
+            submitIcon.click();
+            Thread.sleep(2000);
+        }
 
-    public static void clickCategories() throws InterruptedException {
-        WebElement realEstateElement = driver.findElement(By.xpath("//ion-col[@id='category-selected']//h3[text()='Real Estate']"));
-        realEstateElement.click();
-    }
+        public static void clickSearch () throws InterruptedException {
+            WebElement search = driver.findElement(By.id("btn-search"));
+            search.click();
+        }
 
-    public static void clickAllAds() throws InterruptedException {
-        WebElement allAds = driver.findElement(By.id("btn-list-all-ads"));
-        allAds.click();
-    }
+        public static void clickCategories () throws InterruptedException {
+            WebElement realEstateElement = driver.findElement(By.xpath("//ion-col[@id='category-selected']//h3[text()='Real Estate']"));
+            realEstateElement.click();
+        }
 
-    public static void clickAdvert() throws InterruptedException {
-        WebElement advert = driver.findElement(By.xpath("//h3[text()='208 M2 ARSA ÜZERİNDE SIFIRLANMIŞ 2+1 BAHÇELİ MÜSTAKİL']"));
-        advert.click();
+        public static void clickAllAds () throws InterruptedException {
+            WebElement allAds = driver.findElement(By.id("btn-list-all-ads"));
+            allAds.click();
+        }
 
-    }
+        public static void clickAdvert () throws InterruptedException {
+            WebElement advert = driver.findElement(By.xpath("//h3[text()='208 M2 ARSA ÜZERİNDE SIFIRLANMIŞ 2+1 BAHÇELİ MÜSTAKİL']"));
+            advert.click();
 
-    public static void clickFavorite() throws InterruptedException {
-        WebElement favorite = driver.findElement(By.id("open-favorite-modal"));
-        favorite.click();
-    }
+            List<WebElement> clickadvert = driver.findElements(By.className("advert-container"));
+            clickadvert.get(0).click();
+            Thread.sleep(500);
+
+        }
+
+        public static void clickFavorite () throws InterruptedException {
+            WebElement favorite = driver.findElement(By.id("open-favorite-modal"));
+            favorite.click();
+        }
 
     public static void createNewFavoriteList() throws InterruptedException {
-        WebElement newFavoriteList = driver.findElement(By.cssSelector("#add-new-list"));
-        newFavoriteList.click();
+        WebElement icon = driver.findElement(By.cssSelector("ion-icon[name='add-circle-outline']"));
+        WebElement clickableItem = icon.findElement(By.xpath("./ancestor::ion-item[1]"));
+        clickableItem.click();
+        Thread.sleep(500);
     }
 
     public static void newFavoriteListName() throws InterruptedException {
-        WebElement newName = driver.findElement(By.id("new-favorite-list-name"));
-        newName.click();
+        WebElement input = driver.findElement(By.cssSelector("ion-input#new-favorite-list-name input.native-input"));
+        input.click();
+        input.sendKeys("Test");
         Thread.sleep(500);
-        newName.sendKeys("TEST");
+        WebElement body = driver.findElement(By.tagName("body"));
+        body.click();
+        Thread.sleep(500);
+        WebElement save= driver.findElement(By.id("btn-save-favorite-list"));
+        save.click();
     }
 
-    public static void clickElement(By locator) {
-        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
-        try {
-            element.click();
-        } catch (Exception e) {
-            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+
+    public static void clickElement (By locator){
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            try {
+                element.click();
+            } catch (Exception e) {
+                ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+            }
+        }
+
+        public static void scrollToElement (By locator){
+            WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({behavior:'smooth', block:'center'});", element);
         }
     }
-
-    public static void scrollToElement(By locator) {
-        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({behavior:'smooth', block:'center'});", element);
-    }
-}
